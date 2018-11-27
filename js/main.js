4eadc0d76df074c59d70ee39cb49d69db78870db
Ashton <ashtonenglish3@gmail.com>
2018-11-27 15:45:03 +0200

    made new icons in container3

--- js/main.js
+++ js/main.js
@@ -13,3 +13,11 @@ btn.on('click', function(e) {
   $('html, body').animate({scrollTop:0}, '300');
 });
 
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
+});
+$(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
