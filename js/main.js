bdbef0d7b0103cfbb15f5b14a6f7795c52ef5867
Ashton <ashtonenglish3@gmail.com>
2019-01-17 16:10:04 +0200

    made the outline of my counter

--- js/main.js
+++ js/main.js
@@ -23,3 +23,14 @@ $(window).scroll(function() {
 $(function(){$(".scroll").click(function(){$("html,body").animate({scrollTop:$(".thetop").offset().top},"1000");return false})})
 
 
+$('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 4000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
